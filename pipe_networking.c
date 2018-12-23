2785e417035d6cc795f88df4aa0203fd20a0d7ba
Jason Lin <jasonlin43212@gmail.com>
2018-12-23 14:34:49 -0500

    added multi client server handling

--- pipe_networking.c
+++ pipe_networking.c
@@ -29,33 +29,47 @@ static void sighandler(int signo) {
   =========================*/
 int server_handshake(int *to_client) {
   signal(SIGINT,sighandler);
-  mkfifo("known", 0666);
-  // You need to have both ends open before you can proceed in your code.
-  int up = open("known",O_RDONLY);
+  while (1){
+    mkfifo("known", 0666);
+    // You need to have both ends open before you can proceed in your code.
+    int up = open("known",O_RDONLY);
 
-  char client_message[8];
-  read(up, client_message, 8);
-  printf("Client pipe name: %s\n",client_message);
-  remove("known");
+    char private_pipe[8];
+    read(up, private_pipe, 8);
+    printf("\nClient pipe name: %s\n",private_pipe);
+    remove("known");
 
-  int down = open(client_message, O_WRONLY);
-  *to_client = down;
+    char up_pipe[80] = "up";
+    char down_pipe[80] = "down";
 
-  char client_response[1000] = "";
-  while(1){
-    read(up,client_response,1000);
-    printf("\nClient's message\n===============\n%s\n===============\n",client_response);
-    printf("Sent client their response but uppercased.\nWaiting for next message...\n");
+    strcat(up_pipe,private_pipe);
+    strcat(down_pipe,private_pipe);
 
-    char * send_to_client = malloc(1000);
-    int i;
-    for (i=0; i<(int)strlen(client_response); i++){
-      send_to_client[i] = toupper(client_response[i]);
+    int private_up = open(up_pipe, O_RDONLY);
+    int private_down = open(down_pipe, O_WRONLY);
+    *to_client = private_down;
+
+    int child = fork();
+
+    if (child == 0){
+      char client_response[1000] = "";
+      while(read(private_up,client_response,1000)){
+        printf("\nClient %s message\n===============\n%s\n===============\n",private_pipe,client_response);
+        printf("Sent client %s their response but uppercased.\nWaiting for next message...\n",private_pipe);
+
+        char * send_to_client = malloc(1000);
+        int i;
+        for (i=0; i<(int)strlen(client_response); i++){
+          send_to_client[i] = toupper(client_response[i]);
+        }
+        write(private_down,send_to_client,1000);
+      }
+
+      return up;
     }
-    write(down,send_to_client,1000);
   }
 
-  return up;
+
 }
 
 
@@ -69,27 +83,41 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  mkfifo("private", 0666);
+  char up_pipe[80] = "up";
+  char down_pipe[80] = "down";
+
+  char private_pipe[20] = "";
+  sprintf(private_pipe,"%d",getpid());
+
+  strcat(up_pipe, private_pipe);
+  strcat(down_pipe, private_pipe);
+
+  mkfifo(up_pipe, 0666);
+  mkfifo(down_pipe, 0666);
+
   int up = open("known", O_WRONLY);
-  write(up,"private",sizeof("private"));
-
+  write(up,private_pipe,sizeof(private_pipe));
   *to_server = up;
+  close(up);
 
-  int down = open("private", O_RDONLY);
+  int pipe_up = open(up_pipe, O_WRONLY);
+  int pipe_down = open(down_pipe, O_RDONLY);
 
   char response[1000] = "";
-  remove("private");
+  remove(up_pipe);
+  remove(down_pipe);
+  char message[1000] = "";
+
   while(1){
     printf("Enter message to server: ");
-    char message[1000] = "";
     fgets(message,1000,stdin);
 
     message[strlen(message) -1] = '\0';
-    write(up,message,1000);
+    write(pipe_up,message,1000);
 
-    read(down,response,1000);
+    read(pipe_down,response,1000);
     printf("\nServer Response\n===============\n%s\n===============\n", response);
   }
 
-  return down;
+  return pipe_down;
 }
